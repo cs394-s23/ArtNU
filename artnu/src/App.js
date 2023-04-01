0ccabd1e84965b2470447ff34340488ce5b6f3e8
Mame Coumba Ka <mck@dhcp-10-105-120-206.wireless.northwestern.private>
2023-04-01 17:02:40 -0500

    jsp

--- artnu/src/App.js
+++ artnu/src/App.js
@@ -2,26 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 // import { readPosts } from '../utilities/firebase';
 import { readPosts } from "./firebase.js"
+import Explore from "./components/explore.js"
+
 
 
 function App() {
   readPosts()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Explore/>
     </div>
   );
 }
