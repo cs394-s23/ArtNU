f1b3a38c2f053a3442e2193267bea94c60f00c7c
Lev Rosenberg <lev.rosenberg@gmail.com>
2023-04-01 17:43:40 -0500

    try to add posts

--- artnu/src/components/posts.js
+++ artnu/src/components/posts.js
@@ -1,21 +1,30 @@
 import { readPosts } from "../firebase.js";
 import {useState} from "react";
 
-
+function handlePosts() {
+    readPosts().then(function(posts) {
+        const postsDict = {};
+        posts.forEach((post) => {
+          postsDict[post.id] = post;
+        });
+        return postsDict
+     }).catch(function(error) {
+        console.log(error);
+     });
+    }
 
 export function Posts(){ 
-const data = readPosts().then((data) => {
-    console.log(data)
-    return data
-})
+    const [posts, setPosts] = useState(handlePosts());
+    console.log(posts)
+    return (
+        <div>
+           {posts.map(post => (
+                <div>
+                    {post.img}
+                </div>
+        ))}
+            <h1>Posts</h1>
+        </div>
 
-const [posts, setPosts] = useState(data);
- console.log(posts)
-return (
-
-    <div>
-        <h1>Posts</h1>
-    </div>
-
-)
-}
+    )
+    }
