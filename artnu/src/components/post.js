d4365b0f91334d68494a0ebbb7c17323192eee15
Lev Rosenberg <lev.rosenberg@gmail.com>
2023-04-04 14:52:22 -0500

    some styling and structuring changes

--- artnu/src/components/post.js
+++ artnu/src/components/post.js
@@ -1,21 +1,29 @@
 // CSS file
 
 export function Post(props) {
-    return <div className="post"> 
-            <div className="header">
-                <h3>{props.author}</h3>
-                <h3> ${props.price} </h3>
-            </div>
-            <img src={props.img} width="300" height="300"/>
-            <div className="info">
-                <strong className="likes">{props.likes} likes</strong>
-                <div className="caption">
-                    <p>
-                        <strong>{props.author} </strong>
+    return (
+    <div className="post"> 
+        <div className="header">
+            <h3>{props.author}</h3>
+        </div>
+        <div className = "post-image-box">
+            <img className="post-image" src={props.img} width="300" height="300"/>
+            <p className = "post-price"> ${props.price} </p>
+        </div>
+            <div className = "sub-image">
+                <span className = "info">
+                    <p className = "author"> {props.author} </p>
+                    <div className="likes">
+                        <p> # of likes: {props.likes} </p>
+                        <button> like </button>
+                    </div>
+                </span>
+                <span className = "caption">
                         {props.caption}
-                    </p>
-                </div>
+                </span> 
             </div>
             
-            </div>
+            
+    </div>
+    )
 }
