734f391ded10d5dea83c4301dacd7f9ccacac0ea
Lev Rosenberg <lev.rosenberg@gmail.com>
2023-04-04 12:47:40 -0500

    some preliminary post structuring

--- artnu/src/components/post.js
+++ artnu/src/components/post.js
@@ -2,8 +2,17 @@
 
 export function Post(props) {
     return <div className="post"> 
+            <div className = "post-header">
+                should we have a title here or smth
+            </div>
             <img className='post-image' src={props.img}/>
-            <p> {props.author} </p>
-            <p> {props.price} </p>
+            <div className = "post-icons">
+                icons go here
+            </div>
+            <div className = "post-info">
+                <p> {props.author} </p>
+                <p> ${props.price} </p>
+                <p> likes: {props.likes} </p>
+            </div>
             </div>
 }
