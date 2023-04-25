432806db38c57de6e530bd7370329a9f388b9aeb
tanyapetriv <tatyanapetriv2023@u.northwestern.edu>
2023-04-24 21:59:20 -0500

    user profile

--- artnu/src/firebase.js
+++ artnu/src/firebase.js
@@ -42,7 +42,7 @@ export const provider = new GoogleAuthProvider();
 const database = getDatabase(app);
 
 let settings = {}
-if (process.env.REACT_APP_EMULATE) {
+if (process.env.NODE_ENV == 'production') {
   settings = {
     // Your custom settings
     experimentalForceLongPolling: true,
@@ -57,7 +57,7 @@ const myID= "0mg9bB2gmzmOqwvqanBr";
 
 
 // if (!windows.EMULATION && process.env.NODE_ENV !== 'production' !== 'production') {
-if (process.env.REACT_APP_EMULATE) {
+if (process.env.NODE_ENV == 'production') {
   connectAuthEmulator(auth, "http://127.0.0.1:9099");
   connectDatabaseEmulator(database, "127.0.0.1", 9000);
   connectFirestoreEmulator(db, "127.0.0.1", 8080);
