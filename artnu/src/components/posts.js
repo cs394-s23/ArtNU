306b17189577cec780bd5ad796349a067ed731a1
Callum Bondy <callum.bondy@gmail.com>
2023-04-25 23:53:24 -0500

    filters work

--- artnu/src/components/posts.js
+++ artnu/src/components/posts.js
@@ -1,96 +1,71 @@
-import {useState, useEffect} from "react";
-import {Post} from "./post.js" ;
+import {useState, useEffect, useMemo} from "react";
+import {Post} from "./post.js";
 import {Filters} from "./filters.js";
-import {getDoc} from "firebase/firestore";
-import { db } from "../firebase.js";
-import { onSnapshot, query, collection, orderBy } from "firebase/firestore";
+import {db} from "../firebase.js";
+import {onSnapshot, query, collection, orderBy} from "firebase/firestore";
 
+export function Posts() {
+  const [initialData, setInitialData] = useState([]);
+  const [filter, setFilter] = useState("All Art");
+  const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      query(collection(db, "posts"), orderBy("date", "desc")),
+      (snapshot) => {
+        const postsData = snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          ref: doc.ref,
+          id: doc.id,
+        }));
+        setInitialData(postsData);
+        setPosts(postsData);
+      }
+    );
+    return unsubscribe;
+  }, []);
 
-export function Posts(){
-
-    const [initialData, setInitialData] = useState([])
-    const [filter, setFilter] = useState("All Art");
-    const [posts, setPosts] = useState([])
-
-    useEffect(() => {
-        const unsubscribe = onSnapshot(
-          query(collection(db, "posts"), orderBy("date", "desc")),
-          (snapshot) => {
-            const postsData = snapshot.docs.map((doc) => ({
-              ...doc.data(),
-              ref: doc.ref,
-              id: doc.id,
-            }));
-            setPosts(postsData);
-          }
-        );
-        return unsubscribe;
-      }, []);
-
-    // useEffect(() =>{ // initialize THIS ONLY ONCE
-    // const dp = posts_data
-    // .then(data => {
-    //     setInitialData(data)
-    //     setPosts(data)
-    // })}, []
-    // )
-    
-    function handleFilter(filterType) {
-        setFilter(filterType)
+  function handleFilter(filterType) {
+    if (filter !== filterType) {
+      setFilter(filterType);
     }
-    
-    useEffect(()=>{
-        console.log(filter)
-        if (filter === "All Art") {
-            setPosts([...posts])
-            return
-        }
-        let filteredPosts = initialData.filter(post => post.medium == filter)
-        if (filter == "Commission") {
-            filteredPosts = []
-            initialData.forEach(element => {
-                if(element["tags"]) {
-                    element["tags"].forEach(tag => {
-                        if (tag == "Commission") {
-                            filteredPosts.push(element)
-                        }
-                    });
-                    console.log(element["tags"])
-                }
-            });
-        }
-        // console.log(filteredPosts)
-        setPosts(filteredPosts)
-    }, [filter])
+  }
 
-    return (
-        
-        <div className="feed">
-        <div className="filters">
-            <Filters changeFilter={handleFilter}/>
-        </div>
-        <div className = "postsfeed">
-            {posts.map((post) => (
-                <>
-                    <Post
-                        key = {post.ref}
-                        img={post.img}
-                        author={post.author}
-                        likes={post.likes}
-                        price={post.price}
-                        caption={post.caption}
-                        title = {post.title}
-                        // getUser = {getUser} 
-                        // userRef = {post.user} 
-                        uid={post.uid}
-                    />
-                </>
-            ))}
-        </div>
-        </div>
-        
-
-
-    )
+  const filteredPosts = useMemo(() => {
+    if (filter === "All Art") {
+      return posts;
     }
+
+    let filtered = initialData;
+
+    if (filter === "Commission") {
+      filtered = initialData.filter((post) => post.tags?.includes("Commission"));
+    } else {
+      filtered = initialData.filter((post) => post.medium === filter);
+    }
+
+    return filtered;
+  }, [filter, initialData, posts]);
+
+  return (
+    <div className="feed">
+      <div className="filters">
+        <Filters changeFilter={handleFilter} />
+      </div>
+      <div className="postsfeed">
+        {filteredPosts.map((post) => (
+          <Post
+            key={post.ref}
+            img={post.img}
+            author={post.author}
+            likes={post.likes}
+            price={post.price}
+            caption={post.caption}
+            title={post.title}
+            uid={post.uid}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
